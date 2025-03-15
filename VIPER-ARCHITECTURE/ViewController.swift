d8d0ad0bab031b2569fdff89b67e1aad743b4307
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-03-15 10:24:43 -0500

    Initial Commit

---
+++ VIPER-ARCHITECTURE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER-ARCHITECTURE
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 15/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
